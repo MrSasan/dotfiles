5e1691536164ae99e5d8290a93691dcccde3f75d
Sasan <17885066+MrSasan@users.noreply.github.com>
2023-06-01 19:22:46 +0200

    Add nvim configuration

---
+++ nvim/init.lua
@@ -0,0 +1,20 @@
+-- require other files
+
+require("sasan.plugins-setup") -- keep at the top
+
+require("sasan.core.options")
+require("sasan.core.keymaps")
+require("sasan.core.colorscheme")
+
+require("sasan.plugins.comment")
+require("sasan.plugins.nvim-tree")
+require("sasan.plugins.lualine")
+require("sasan.plugins.telescope")
+require("sasan.plugins.nvim-cmp")
+require("sasan.plugins.lsp.mason")
+require("sasan.plugins.lsp.lspsaga")
+require("sasan.plugins.lsp.lspconfig")
+require("sasan.plugins.lsp.null-ls")
+require("sasan.plugins.autopairs")
+require("sasan.plugins.treesitter")
+require("sasan.plugins.gitsigns")
