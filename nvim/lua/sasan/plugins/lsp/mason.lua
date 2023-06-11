df0706dc07bcc8c7fa2d8acafd7813fbce7849a2
Sasan <17885066+MrSasan@users.noreply.github.com>
2023-06-11 23:48:08 +0200

    fix Mason/lsp config files for nvim

--- nvim/lua/sasan/plugins/lsp/mason.lua
+++ nvim/lua/sasan/plugins/lsp/mason.lua
@@ -1,45 +1,44 @@
 local mason_status, mason = pcall(require, "mason")
 if not mason_status then
-  return
+	return
 end
 
 local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_status then
-  return
+	return
 end
 
 -- import mason-null-ls plugin safely
 local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
 if not mason_null_ls_status then
-  return
+	return
 end
-
 
 mason.setup()
 
 mason_lspconfig.setup({
-  ensure_installed = {
-  "tsserver",
-  "html",
-  "cssls",
-  "tailwindcss",
-  "sumneko_lua",
-  "clangd",
-  "csharp_ls",
-  "texlab",
-  "marksman",
-  "pylsp",
-  "sqls",
-  "svelte",
-  "tsserver",
-  "lemminx",
-  }
+	ensure_installed = {
+		"tsserver",
+		"html",
+		--  "cssls",
+		"tailwindcss",
+		--		"sumneko_lua",
+		"clangd",
+		--  "csharp_ls",
+		"texlab",
+		"marksman",
+		"pylsp",
+		--		"sqls",
+		"svelte",
+		"tsserver",
+		"lemminx",
+	},
 })
 
 mason_null_ls.setup({
-  ensure_installed = {
-    "prettier",
-    "stylua",
-    "eslint_d",
-  }
+	ensure_installed = {
+		"prettier",
+		"stylua",
+		"eslint_d",
+	},
 })
